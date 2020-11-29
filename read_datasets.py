43dee94ce278e819c7494cdf346ac01e74d6697a
Jose Juan <jose.juan.galan@estudiantat.upc.edu>
2020-11-29 19:29:01 +0100

    Update read_datasets.py

--- read_datasets.py
+++ read_datasets.py
@@ -30,30 +30,36 @@ def read_vowel_fold(i):
     return vowel_train, vowel_test
 
 
-def preprocess_adult(data):
-    adult_df = pd.DataFrame(data)
+def preprocess_adult(train_data, test_data):
+    test_df = pd.DataFrame(test_data)
+    train_df = pd.DataFrame(train_data)
     # Replace missing values
-    adult_df = adult_df.replace(b"?", np.nan)
+    train_df = train_df.replace(b"?", np.nan)
+    test_df = test_df.replace(b"?", np.nan)
     # Handle missing values (for now, just delete the row with the missing value)
     # The index can be reset after dropna() by calling reset_index(drop=True)
     # The index reset is omitted for now in order not to lose track of the missing values dropped
-    adult_df = adult_df.dropna()
+    train_df = train_df.dropna()
+    test_df = test_df.dropna()
     # Encode classes [b'<=50K', b'>50K'] to numbers
     enc = LabelEncoder()
-    adult_df["class"] = enc.fit_transform(adult_df["class"]).astype(float)
+    train_df["class"] = enc.fit_transform(train_df["class"]).astype(float)
+    test_df["class"] = enc.fit_transform(test_df["class"]).astype(float)
     # One-hot encoding the categorical attributes
+    n = len(train_df)
+    adult_df = pd.concat([train_df, test_df])
     adult_df = pd.get_dummies(adult_df)
     # Normalize the data
     scaler = preprocessing.MinMaxScaler()
     adult_df_scaled = scaler.fit_transform(adult_df.values)
     adult_df = pd.DataFrame(adult_df_scaled)
-    return adult_df.to_numpy()
+    adult_np = adult_df.to_numpy()
+    return adult_np[:n], adult_np[n:]
 
 
 def read_adult_fold(i):
     train_data, train_meta = arff.loadarff('./datasets/adult/adult.fold.00000'+str(i)+'.train.arff')
     test_data, test_meta = arff.loadarff('./datasets/adult/adult.fold.00000'+str(i)+'.test.arff')
-    adult_train = preprocess_adult(train_data)
-    adult_test = preprocess_adult(test_data)
+    adult_train, adult_test = preprocess_adult(train_data, test_data)
     return adult_train, adult_test
 
