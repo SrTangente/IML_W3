a8837cdda07e7a47090056b48f27caa35c816597
Priwinn <43821596+Priwinn@users.noreply.github.com>
2020-12-18 18:16:38 +0100

    knn refactor and classify

--- classify.py
+++ classify.py
@@ -1,10 +1,10 @@
 from read_datasets import *
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from knn import kNNAlgorithm
 from visualize import visualize
+import pandas as pd
 
 
-def classify(dataset, k=5, show=False, reduction_alg=None):
+def classify(dataset, k=1, r=1, w='eq', v='maj', show=False, reduction_alg=None):
     """
     Execute a 10-fold kNN.
     :param dataset: either "adult" or "vowel"
@@ -12,18 +12,21 @@ def classify(dataset, k=5, show=False, reduction_alg=None):
     :param show: boolean, whether to show a 2-plot of the results
     :return: None
     """
-
+    df=pd.DataFrame(columns=['dataset','k','r','w','v','reduction','acc','eff','storage'])
     read = read_vowel_fold if dataset == 'vowel' else read_adult_fold
     for i in range(10):
         x_train, y_train, x_test, y_test = read(i)
         if reduction_alg:
+            n0=x_train.shape[0]
             x_train, y_train = reduction_alg(x_train, y_train, k)
+            storage=x_train.shape[0]/n0
+        else:
+            storage=1
         # Call here the KNN
-        knn = KNeighborsClassifier(n_neighbors=k)
-        knn.fit(x_train, y_train)
-        y_pred = knn.predict(x_test)
+        indexes, y_pred, eff, acc = kNNAlgorithm(x_train,y_train, x_test,y_test, k, r, w, v)
 
-        print(f"Iteration {i}: {accuracy_score(y_test, y_pred)}")
-
+        print(f"{dataset} fold {i}, k={k}, r={r}, w={w}, v={v}: Accuracy {acc}, Time {eff}")
+        df=df.append(pd.DataFrame([[dataset,k,r,w,v,reduction_alg,acc,eff,storage]],columns=['dataset','k','r','w','v','reduction','acc','eff','storage']))
         if show:
             visualize(x_test, y_test, y_pred)
+    return df
