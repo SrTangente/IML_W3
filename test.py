bdea2a1ea003a7f380695dc3c99ee26d9aa34bcc
SrTangente <josejuangalan@gmail.com>
2020-12-21 00:56:16 +0100

    added results

--- test.py
+++ test.py
@@ -4,16 +4,6 @@ from fcnn import fcnn
 from menn import menn
 
 df = pd.DataFrame()
-for k in [1,3,5,7]:
-    df=pd.concat([df,classify_fast('satimage', 5, 1, 'eq', 'shep', reduction_alg=menn)])
-df.to_csv('satimage_menn.csv')
-
-df = pd.DataFrame()
-for k in [1,3,5, 7]:
-    df=pd.concat([df,classify_fast('vowel',1, 1.5, 'mi', 'maj', reduction_alg=menn)])
-df.to_csv('vowel_menn.csv')
-
-df = pd.DataFrame()
 df=pd.concat([df,classify_fast('satimage', 5, 1, 'eq', 'shep', reduction_alg=fcnn)])
 df.to_csv('satimage_fcnn.csv')
 
@@ -22,14 +12,19 @@ df=pd.concat([df,classify_fast('vowel',1, 1.5, 'mi', 'maj', reduction_alg=fcnn)]
 df.to_csv('vowel_fcnn.csv')
 
 df = pd.DataFrame()
-for k in [3]:
-    df=pd.concat([df,classify_fast('satimage', 5, 1, 'eq', 'shep', reduction_alg=fcnn)])
+for k in [1,3,5,7]:
+    df=pd.concat([df,classify_fast('satimage', k, 1, 'eq', 'shep', reduction_alg=menn)])
+df.to_csv('satimage_menn.csv')
+
+df = pd.DataFrame()
+for k in [1,3,5, 7]:
+    df=pd.concat([df,classify_fast('vowel',k, 1.5, 'mi', 'maj', reduction_alg=menn)])
+df.to_csv('vowel_menn.csv')
+
+df = pd.DataFrame()
+df=pd.concat([df,classify_fast('satimage',3, 1, 'eq', 'shep', reduction_alg=drop3)])
 df.to_csv('satimage_drop3_3.csv')
 
 df = pd.DataFrame()
-for k in [3]:
-    df=pd.concat([df,classify_fast('vowel',1, 1.5, 'mi', 'maj', reduction_alg=fcnn)])
+df=pd.concat([df,classify_fast('vowel',3, 1.5, 'mi', 'maj', reduction_alg=drop3)])
 df.to_csv('vowel_drop3_3.csv')
-
-dfs = classify('satimage', k=5, r=1, w='eq', v='shep', reduction_alg=drop3, show=False)
-dfs.to_csv('satimage_drop3.csv')
