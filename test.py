53045ee035c6f807e6d09e49d787443c79516bd0
Jose Juan <jose.juan.galan@estudiantat.upc.edu>
2020-12-20 19:48:34 +0100

    added analysis file

--- test.py
+++ test.py
@@ -2,20 +2,8 @@ from classify import *
 from drop import drop2, drop3
 from fcnn import fcnn
 
-classify('vowel', k=1, r=1.5, w='mi', v='maj', reduction_alg=drop2, show=False)
+df = classify('vowel', k=1, r=1.5, w='mi', v='maj', reduction_alg=drop3, show=False)
+df.to_csv('vowel_drop3.csv')
 
-results = pd.read_csv('results.csv')
-
-sat = results[results["dataset"]=='satimage']
-sat_results = sat.groupby(['dataset', 'k', 'r', 'w', 'v']).mean()
-print('Best combination for satimage')
-print(sat_results.iloc[np.argmax(sat_results["acc"])])
-print('--------------')
-
-vow = results[results["dataset"]=='vowel']
-vow_results = vow.groupby(['dataset', 'k', 'r', 'w', 'v']).mean()
-print('Best combination for vowel')
-print(vow_results.iloc[np.argmax(vow_results["acc"])])
-
-#classify('vowel', k=5, show=False, reduction_alg=drop3)
+classify('vowel', k=5, show=False, reduction_alg=drop3)
 classify('vowel', k=5, show=False, reduction_alg=fcnn)
